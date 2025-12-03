bfd2b8a51f215b5535c0f9fb711f0b2f4380833e
Aristotelis Ntouros <n0nic@hotmail.com>
2025-12-03 20:29:23 +0200

    Simplify test categories using TraitAttribute

--- src/BookstoreApiTests.Tests/Infrastructure/TestCategories.cs
+++ src/BookstoreApiTests.Tests/Infrastructure/TestCategories.cs
@@ -10,29 +10,17 @@ public static class TestCategories
     public const string Performance = "Performance";
 }
 
-[TraitDiscoverer("BookstoreApiTests.Tests.Infrastructure.CategoryTraitDiscoverer", "BookstoreApiTests.Tests")]
-[AttributeUsage(AttributeTargets.Method | AttributeTargets.Class, AllowMultiple = true)]
-public class SmokeTestAttribute : Attribute, ITraitAttribute { }
-
-[TraitDiscoverer("BookstoreApiTests.Tests.Infrastructure.CategoryTraitDiscoverer", "BookstoreApiTests.Tests")]
-[AttributeUsage(AttributeTargets.Method | AttributeTargets.Class, AllowMultiple = true)]
-public class RegressionTestAttribute : Attribute, ITraitAttribute { }
-
-[TraitDiscoverer("BookstoreApiTests.Tests.Infrastructure.CategoryTraitDiscoverer", "BookstoreApiTests.Tests")]
-[AttributeUsage(AttributeTargets.Method | AttributeTargets.Class, AllowMultiple = true)]
-public class PerformanceTestAttribute : Attribute, ITraitAttribute { }
-
-public class CategoryTraitDiscoverer : ITraitDiscoverer
+public class SmokeTestAttribute : TraitAttribute
 {
-    public IEnumerable<KeyValuePair<string, string>> GetTraits(IAttributeInfo traitAttribute)
-    {
-        var attributeTypeName = traitAttribute.AttributeType.Name;
+    public SmokeTestAttribute() : base("Category", TestCategories.Smoke) { }
+}
 
-        if (attributeTypeName == nameof(SmokeTestAttribute))
-            yield return new KeyValuePair<string, string>("Category", TestCategories.Smoke);
-        else if (attributeTypeName == nameof(RegressionTestAttribute))
-            yield return new KeyValuePair<string, string>("Category", TestCategories.Regression);
-        else if (attributeTypeName == nameof(PerformanceTestAttribute))
-            yield return new KeyValuePair<string, string>("Category", TestCategories.Performance);
-    }
+public class RegressionTestAttribute : TraitAttribute
+{
+    public RegressionTestAttribute() : base("Category", TestCategories.Regression) { }
+}
+
+public class PerformanceTestAttribute : TraitAttribute
+{
+    public PerformanceTestAttribute() : base("Category", TestCategories.Performance) { }
 }
