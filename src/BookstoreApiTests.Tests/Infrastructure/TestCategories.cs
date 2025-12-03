6e0449701047089bb2dac12c1bc715bf4b7a2349
Aristotelis Ntouros <n0nic@hotmail.com>
2025-12-03 20:33:12 +0200

    Fix test categories with ITraitAttribute

--- src/BookstoreApiTests.Tests/Infrastructure/TestCategories.cs
+++ src/BookstoreApiTests.Tests/Infrastructure/TestCategories.cs
@@ -1,3 +1,4 @@
+using Xunit.Abstractions;
 using Xunit.Sdk;
 
 namespace BookstoreApiTests.Tests.Infrastructure;
@@ -10,17 +11,38 @@ public static class TestCategories
     public const string Performance = "Performance";
 }
 
-public class SmokeTestAttribute : TraitAttribute
+[TraitDiscoverer("BookstoreApiTests.Tests.Infrastructure.SmokeTraitDiscoverer", "BookstoreApiTests.Tests")]
+[AttributeUsage(AttributeTargets.Method | AttributeTargets.Class, AllowMultiple = true)]
+public class SmokeTestAttribute : Attribute, ITraitAttribute { }
+
+[TraitDiscoverer("BookstoreApiTests.Tests.Infrastructure.RegressionTraitDiscoverer", "BookstoreApiTests.Tests")]
+[AttributeUsage(AttributeTargets.Method | AttributeTargets.Class, AllowMultiple = true)]
+public class RegressionTestAttribute : Attribute, ITraitAttribute { }
+
+[TraitDiscoverer("BookstoreApiTests.Tests.Infrastructure.PerformanceTraitDiscoverer", "BookstoreApiTests.Tests")]
+[AttributeUsage(AttributeTargets.Method | AttributeTargets.Class, AllowMultiple = true)]
+public class PerformanceTestAttribute : Attribute, ITraitAttribute { }
+
+public class SmokeTraitDiscoverer : ITraitDiscoverer
 {
-    public SmokeTestAttribute() : base("Category", TestCategories.Smoke) { }
+    public IEnumerable<KeyValuePair<string, string>> GetTraits(IAttributeInfo traitAttribute)
+    {
+        yield return new KeyValuePair<string, string>("Category", TestCategories.Smoke);
+    }
 }
 
-public class RegressionTestAttribute : TraitAttribute
+public class RegressionTraitDiscoverer : ITraitDiscoverer
 {
-    public RegressionTestAttribute() : base("Category", TestCategories.Regression) { }
+    public IEnumerable<KeyValuePair<string, string>> GetTraits(IAttributeInfo traitAttribute)
+    {
+        yield return new KeyValuePair<string, string>("Category", TestCategories.Regression);
+    }
 }
 
-public class PerformanceTestAttribute : TraitAttribute
+public class PerformanceTraitDiscoverer : ITraitDiscoverer
 {
-    public PerformanceTestAttribute() : base("Category", TestCategories.Performance) { }
+    public IEnumerable<KeyValuePair<string, string>> GetTraits(IAttributeInfo traitAttribute)
+    {
+        yield return new KeyValuePair<string, string>("Category", TestCategories.Performance);
+    }
 }
